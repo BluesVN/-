ed39baa13897e6950cb64512725494417b893531
Dylan <476618360@qq.com>
2018-08-05 23:01:16 +0800

    header commit

--- src/pages/jobdetail/components/Header.vue
+++ src/pages/jobdetail/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <img src="image/logo.png" >
+  <div class="header">
+    <div class="img-block">
+      <img src="../../../assets/image/logo.png" >
+    </div>
+    <span class="text">职引官</span>
+    <div class="header-right">
+      <span>首页</span>
+      <span>引享圈</span>
+      <span>职位行情</span>
+      <span>测一测</span>
+      <span class="ask border">我要提问</span>
+    </div>
   </div>
 </template>
 <script>
@@ -9,6 +19,36 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-//@import '~styles/variables'
-
+@import '~styles/variables'
+.header
+  display flex
+  height 1rem
+  margin-top .15rem
+  padding-left 15%
+  background-color #404040
+  justify-content: start;
+  align-items: center;
+  color #fff
+  .img-block
+    display inline-block
+    background-color $logoColor
+    min-width 2rem
+    height 1.3rem
+    line-height 1.3rem
+    margin-top -.15rem
+    text-align center
+  .text
+    margin-left .1rem
+    white-space: nowrap;
+  .header-right
+    position: absolute;
+    height: 1rem;
+    line-height: 1rem;
+    font-size: 0.18rem;
+    overflow: hidden;
+    right: 5%;
+  .header-right span
+    margin-left .5rem
+  .ask
+    padding .1rem
 </style>
